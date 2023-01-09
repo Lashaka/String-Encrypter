d2fcb22a1926e6e170e83441831f3699e220a0db
Victor Rutskin <Lashaka250375@gmail.com>
2023-01-09 14:09:11 +0200

    Delete Trash.py

--- Trash.py
+++
@@ -1,6 +0,0 @@
-#1. need to make this work with given key most important
-# when encrypting using a key, if too long do not use excess, if too short loop on it. GREAT IDEA!! will fix many issues :D
-
-## 2. make a class that adds trash to the string, and knows how to add/remove from it, random the amount of trash? make trash more complex?
-
-## what data does an image made of? optional : encrypt data in an image?
